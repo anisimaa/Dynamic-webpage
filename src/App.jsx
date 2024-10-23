b5ebcf54922b915314f92fb46b3bbced46080047
anisimaa <isla.anisimaa00@gmail.com>
2024-10-23 19:18:43 +0300

    tested component assignments header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import header from './components/header'
+import footer from './components/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Header />   {/* Use the Header component */}
+          <main>
+              <h2>This is the content for the Component Assignment</h2>
+              {/* Add more components as needed */}
+          </main>
+          <Footer />   {/* Use the Footer component */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
